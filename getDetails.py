b3bbaad115dee0158fd746f8cbecd87d799782b5
jenifermajella02 <62872645+jenifermajella02@users.noreply.github.com>
2022-06-15 16:46:53 +0530

    Included dry_run and comments

--- getDetails.py
+++ getDetails.py
@@ -1,20 +1,30 @@
 from netmiko import ConnectHandler
 
-cisco1 = {
-    "device_type": "cisco_ios",
-    "host": "ios-xe-mgmt.cisco.com",
-    "username": "developer",
-    "password": "C1sco12345",
-}
-
-# Show command that we execute
-command = "show interfaces summary"
-with ConnectHandler(**cisco1) as net_connect:
-    output = net_connect.send_command(command)
-
-# Automatically cleans-up the output so that only the show output is returned
-print()
-print(output)
-print()
+global dry_run
+dry_run = False
 
 
+################################################################################
+# Name: interface_details()
+# Description: This function is used for retrieving interface information from
+#              network device
+# Input Parameter: none
+# Output Parameter: interface details from device
+################################################################################
+def interface_details():
+    cisco1 = {
+        "device_type": "cisco_ios",
+        "host": "ios-xe-mgmt.cisco.com",
+        "username": "developer",
+        "password": "C1sco12345",
+    }
+    # Show command that we execute.
+    command = "show interfaces summary"
+    if not dry_run:
+        with ConnectHandler(**cisco1) as net_connect:
+            output = net_connect.send_command(command)
+        # Automatically cleans-up the output so that only the show output is returned
+        print(output)
+        return output
+    else:
+        print('DRY_RUN enabled')
