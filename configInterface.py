c8f8bf4de54248bb85416d489e8470da64e88577
jenifermajella02 <62872645+jenifermajella02@users.noreply.github.com>
2022-06-15 16:49:09 +0530

    Added comments and dry_run

--- configInterface.py
+++ configInterface.py
@@ -1,39 +1,77 @@
 from ncclient import manager
 
-creation_rpc = '''
-<config>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-      <interface>
-                <Loopback>
-          <name>300</name>
-          <ip>
-            <address>
-              <primary>
-                <address>192.168.100.100</address>
-                <mask>255.255.255.255</mask>
-              </primary>
-            </address>
-          </ip>
-        </Loopback>
-      </interface>
-    </native>
-</config>
-      '''
-deletion_rpc = '''
-<config>
-  <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-      <interface>
-          <Loopback xmlns:nc="urn:ietf:params:xml:ns:netconf:base:1.0" nc:operation="delete">
-          <name>300</name>
-          </Loopback>
-      </interface>
-    </native>
-</config>
-'''
+global dry_run
+dry_run = False
 
-with manager.connect(host="ios-xe-mgmt.cisco.com", port=830, username='developer', password='C1sco12345',hostkey_verify=False) as m:
-  reply = m.edit_config(creation_rpc, target='running')
-  print(reply)
-  reply = m.edit_config(deletion_rpc, target='running')
-  print(reply)
 
+################################################################################
+# Name: create_interface()
+# Description: This function is used for configuring the loopback IP on the
+#              network device
+# Input Parameter: loopback ip,name
+# Output Parameter: ok reply from device
+################################################################################
+def create_interface(name, ipvalue):
+    creation_rpc = '''
+   <config>
+     <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+         <interface>
+                   <Loopback>
+             <name>%s</name>
+             <ip>
+               <address>
+                 <primary>
+                   <address>%s</address>
+                   <mask>255.255.255.255</mask>
+                 </primary>
+               </address>
+             </ip>
+           </Loopback>
+         </interface>
+       </native>
+   </config>
+   '''
+    rpc_msg = creation_rpc % (name, ipvalue)
+    print(rpc_msg)
+    if not dry_run:
+        with manager.connect(host="ios-xe-mgmt.cisco.com", port=830, username='developer', password='C1sco12345',
+                             hostkey_verify=False) as m:
+            reply = m.edit_config(rpc_msg, target='running')
+            print(reply)
+    else:
+        print('DRY_RUN enabled')
+        print(rpc_msg)
+
+
+################################################################################
+# Name: del_interface()
+# Description: This function is used for deleting the loopback IP on the
+#              network device
+# Input Parameter: name
+# Output Parameter: ok reply from device
+################################################################################
+def del_interface(name):
+    print(name)
+    name = name
+    deletion_rpc = '''
+   <config>
+     <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+         <interface>
+             <Loopback xmlns:nc="urn:ietf:params:xml:ns:netconf:base:1.0" nc:operation="delete">
+             <name>%s</name>
+             </Loopback>
+         </interface>
+       </native>
+   </config>
+   '''
+
+    rpc_msg = deletion_rpc % name
+    print(rpc_msg)
+    if not dry_run:
+        with manager.connect(host="ios-xe-mgmt.cisco.com", port=830, username='developer', password='C1sco12345',
+                             hostkey_verify=False) as m:
+            reply = m.edit_config(rpc_msg, target='running')
+            print(reply)
+    else:
+        print('DRY_RUN enabled')
+        print(rpc_msg)
